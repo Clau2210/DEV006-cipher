b6c1c4c1b72aa2ccdc367f25fd1dcc119a8305f2
Clau2210 <claudiauriascaudillo@gmail.com>
2023-03-20 19:27:07 -0600

    Modificando index.js_1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+const buttonencode=document.getElementById("button_encode");
+buttonencode.addEventListener("click", (encode)=> {
+  console.log(cipher.encode());
+});
+
+const buttondecode=document.getElementById("button_decode");
+buttondecode.addEventListener("click", (decode)=> {
+  console.log(cipher.decode());
+});
+
