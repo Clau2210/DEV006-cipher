e422faed3d4b9ce4ec1ff1d2657c38ec90a837ba
Clau2210 <claudiauriascaudillo@gmail.com>
2023-03-23 10:53:52 -0600

    Cambios en index, cipher y css

--- src/cipher.js
+++ src/cipher.js
@@ -1,45 +1,46 @@
 const cipher = {
-  encode: ()=>{
-    const messagencode =document.getElementById("message_encode").value;
-    let messagencode2="";
+  encode: function(offset,messagencode){
     
+    if (typeof Number(offset) !== "number" || typeof messagencode !== "string"){
+      throw new TypeError("Debes capturar números y letras.");
+    }  
+    let messagencode2="";
     for (let i=0; i<messagencode.length;i++){
       const letterCode=messagencode[i].charCodeAt();
-      const offset=document.getElementById("position_encode").value;
-     
+      
       if (letterCode >= 91 && letterCode <= 126){
         window.alert('¡Sólo letras mayúsculas excepto Ñ !');
-      
-          
       } 
       else if(letterCode >= 65 && letterCode <= 90){
-       messagencode2 += String.fromCharCode((letterCode - 65 + Number(offset) % 26) + 65);
-        
-    
+        messagencode2 += String.fromCharCode(((letterCode - 65 + Number(offset)) % 26) + 65);
       } 
-  
-   
+      
     }
-        console.log(messagencode2);    
-    alert(messagencode2);
+    return messagencode2;
+  
   }, 
-  decode:()=> {
-    const messagedecode=document.getElementById("message_decode").value;
+  decode: function(offset, messagedecode){
+    if (typeof Number(offset) !== "number" || typeof messagedecode !== "string"){
+      throw new TypeError("Debes capturar números y letras.");
+    } 
+    while (offset > 26){
+      offset -= 26;
+    }
     let messagedecode2="";
     for (let i=0; i<messagedecode.length;i++){
       const letterCode=messagedecode[i].charCodeAt();
-      const offset=document.getElementById("position_decode").value;
       if (letterCode >=91 && letterCode <=126){
         window.alert('¡Sólo letras mayúsculas excepto Ñ!');
       }
       else if (letterCode >=65 && letterCode <=90){
-        messagedecode2 += String.fromCharCode((letterCode - 65 -Number(offset) % 26) + 65);
+        messagedecode2 += String.fromCharCode(((letterCode - 65 -Number(offset)+26) % 26) + 65);
       }
     }
-    console.log(messagedecode2);     
-    alert(messagedecode2);
+    return messagedecode2;
+   
+
   
-}
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
 
 const buttonencode=document.getElementById("button_encode");
-buttonencode.addEventListener("click", (encode)=> {
-  console.log(cipher.encode());
+
+buttonencode.addEventListener("click", ()=> {
+  
+  const messagencode =document.getElementById("message_encode").value;
+  const offset=document.getElementById("position_encode").value;
+  const result=cipher.encode(offset, messagencode);
+  window.alert(result);
+  return result;
+  
+  //console.log(messagencode3);
+  //console.log(cipher.encode());
 });
 
 const buttondecode=document.getElementById("button_decode");
-buttondecode.addEventListener("click", (decode)=> {
-  console.log(cipher.decode());
+buttondecode.addEventListener("click", ()=> {
+  const messagedecode=document.getElementById("message_decode").value;
+  const offset=document.getElementById("position_decode").value;
+  const result2=cipher.decode(offset, messagedecode);
+  window.alert(result2);
+  return(result2);
+  //console.log(cipher.decode());
 });
 
