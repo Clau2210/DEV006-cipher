53de039fbc07ef5102f73f941b044cb1a3eea357
Clau2210 <claudiauriascaudillo@gmail.com>
2023-03-24 09:13:26 -0600

    Última modificación

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,19 @@
 const cipher = {
   encode: function(offset,messagencode){
-    
     if (typeof Number(offset) !== "number" || typeof messagencode !== "string"){
       throw new TypeError("Debes capturar números y letras.");
     }  
     let messagencode2="";
     for (let i=0; i<messagencode.length;i++){
       const letterCode=messagencode[i].charCodeAt();
-      
       if (letterCode >= 91 && letterCode <= 126){
         window.alert('¡Sólo letras mayúsculas excepto Ñ !');
       } 
       else if(letterCode >= 65 && letterCode <= 90){
         messagencode2 += String.fromCharCode(((letterCode - 65 + Number(offset)) % 26) + 65);
-      } 
-      
+      }  
     }
     return messagencode2;
-  
   }, 
   decode: function(offset, messagedecode){
     if (typeof Number(offset) !== "number" || typeof messagedecode !== "string"){
@@ -37,10 +33,6 @@ const cipher = {
       }
     }
     return messagedecode2;
-   
-
-  
   }
 };
-
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import cipher from './cipher.js';
-
-//console.log(cipher);
-
 const buttonencode=document.getElementById("button_encode");
-
 buttonencode.addEventListener("click", ()=> {
-  
   const messagencode =document.getElementById("message_encode").value;
   const offset=document.getElementById("position_encode").value;
   const result=cipher.encode(offset, messagencode);
   window.alert(result);
   return result;
-  
-  //console.log(messagencode3);
-  //console.log(cipher.encode());
 });
-
 const buttondecode=document.getElementById("button_decode");
 buttondecode.addEventListener("click", ()=> {
   const messagedecode=document.getElementById("message_decode").value;
@@ -23,6 +14,5 @@ buttondecode.addEventListener("click", ()=> {
   const result2=cipher.decode(offset, messagedecode);
   window.alert(result2);
   return(result2);
-  //console.log(cipher.decode());
 });
 
